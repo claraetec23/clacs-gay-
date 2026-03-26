d71743963dcb5dc82fc51341cabf9c901aa16d2a
claraetec23 <claraetec23@gmail.com>
2026-03-26 10:46:55 +0000

    fix: URL da API dinâmico para GitHub Codespaces

--- APP/src/config/env.js
+++ APP/src/config/env.js
@@ -1,1 +1,5 @@
-export const API_URL = "https://fuzzy-broccoli-v6jgj4r69f69wq-3000.app.github.dev";
+const isGitHubDev = window.location.hostname.endsWith(".app.github.dev");
+
+export const API_URL = isGitHubDev
+    ? "https://" + window.location.hostname.replace(/-40413\.app\.github\.dev$/, "-3000.app.github.dev")
+    : "http://localhost:3000";
